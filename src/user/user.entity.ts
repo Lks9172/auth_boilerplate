9be8261c9d9050f281ca4f4bae12a8b34a530c07
Lks9172 <ekdan9172@naver.com>
2022-09-13 00:02:54 +0900

    feat: user entity role컬럼에 index 설정

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -9,19 +9,18 @@ import {
 @Entity()
 export class User extends BaseEntity {
   @Index()
+  @Column('varchar', { name: 'role', nullable: false, length: 20, default: 'basicuser'})
+  role!: string
+
+  @Index()
   @PrimaryColumn('varchar', {name: 'user_id', nullable: false, unique: true, length: 20})
   userId!: string
 
   @Column('char', { name: 'password', nullable: false, length: 60})
   password!: string
 
-  @Column('varchar', { name: 'role', nullable: false, length: 20, default: 'basicuser'})
-  role!: string
-
-
   @Column('varchar', { name: 'accessToken', nullable: true, length: 256, default: null })
   accessToken!: string
-
 
   @Column('varchar', { name: 'refresh_token', nullable: true, length: 256, default: null })
   refreshToken!: string
