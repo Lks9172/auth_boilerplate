56d5cb4f8d4d18f4a059d8842f1fa1d24ef623f8
Lks9172 <ekdan9172@naver.com>
2022-10-04 01:39:09 +0900

    docs: README.md 작성

---
+++ README.md
@@ -0,0 +1,24 @@
+## Description
+nest프레임워크를 사용한 auth 보일러 플레이트
+
+### Installation
+```
+npm install
+```
+
+### 실행
+```
+npm start
+```
+
+### test
+
+# unit tests
+```
+npm run test
+```
+
+# test coverage
+```
+npm run test:cov
+```
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,15 +1,15 @@
-import * as dotenv from "dotenv";
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as dotenv from 'dotenv';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 dotenv.config();
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: "mysql",
+  type: 'mysql',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [__dirname + "/../**/*entity.{js,ts}"],
+  entities: [__dirname + '/../**/*entity.{js,ts}'],
   synchronize: true,
 };
