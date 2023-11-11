27d56860f274df44f7d66d33f2bb5124ddcae606
Lks9172 <ekdan9172@naver.com>
2023-11-11 12:05:44 +0900

    feat: UserRepository.createUser에 트랜잭션 적용
    - EntityManager.transaction메서드를 사용해 트랜잭션 적용

--- src/user/repository/user.repository.ts
+++ src/user/repository/user.repository.ts
@@ -1,27 +1,37 @@
-import { BadRequestException, NotFoundException } from '@nestjs/common';
-import { EntityRepository, Repository } from 'typeorm';
-import { Account } from '../application/account.class';
+import { BadRequestException } from '@nestjs/common';
+import { EntityRepository, Repository, EntityManager } from 'typeorm';
 import { User } from '../domain/user.entity';
+import { UserInfo } from '../application/builder/user.builder';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   /**한개의 user 생성 */
-  async createUser(account: Account): Promise<User> {
+  async createUser(accountInfo: UserInfo): Promise<User> {
+    const { userId, password, email, name, birthDate, gender } = accountInfo;
+
+    const entityManager: EntityManager = this.manager;
     const checkUser = await this.findOne({
-      userId: account.userId,
-    });
-    if (checkUser) throw new BadRequestException('이미 존재하는 userId입니다.');
-
-    const user = this.create({
-      userId: account.userId,
-      password: account.cipher.hashedPw,
-      email: account.email,
-      name: account.name,
-      birthDate: account.birthdate,
-      gender: account.gender
+      userId,
     });
 
-    await this.save(user);
-    return user;
+    if (checkUser) {
+      throw new BadRequestException('이미 존재하는 userId입니다.');
+    }
+
+    return entityManager.transaction(async transactionalEntityManager => {
+      const userRepository = transactionalEntityManager.getRepository(User);
+      const user = userRepository.create({
+        userId,
+        password,
+        email,
+        name,
+        birthDate,
+        gender,
+      });
+
+      await userRepository.save(user);
+
+      return user;
+    });
   }
 }
