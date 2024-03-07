d45ab4c8066f83075020e26d5e30df1d3abb1186
Lks9172 <ekdan9172@naver.com>
2024-03-07 22:38:43 +0900

    feat: user엔티티 수정

--- src/user/domain/user.entity.ts
+++ src/user/domain/user.entity.ts
@@ -1,44 +1,96 @@
-import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+import { 
+  AfterLoad, 
+  BeforeInsert, 
+  BeforeUpdate, 
+  Column, 
+  CreateDateColumn,
+  DeleteDateColumn, 
+  Entity, 
+  Index, 
+  ManyToOne, 
+  PrimaryGeneratedColumn, 
+  UpdateDateColumn 
+  } from 'typeorm';
+  import * as bcrypt from 'bcryptjs';
+  import { Exclude, Expose } from 'class-transformer';
+import { AuthProvidersEnum } from '../../auth/auth-providers.enum';
+import { FileEntity } from '../../files/entities/file.entity';
+import { Role } from '../../roles/entities/role.entity';
+import { Status } from '../../statuses/entities/status.entity';
+import { EntityHelper } from '../../utils/entity-helper';
 
 @Entity()
-export class User extends BaseEntity {
-  @PrimaryColumn('varchar', {
+export class User extends EntityHelper {
+  @PrimaryGeneratedColumn({
     name: 'user_id',
-    nullable: false,
-    unique: true,
-    length: 20,
   })
-  userId!: string;
+  id!: number;
 
-  @Column('char', { name: 'password', nullable: true, default: null, length: 60 })
-  password!: string | null;
+  // For "string | null" we need to use String type.
+  // More info: https://github.com/typeorm/typeorm/issues/2567
+  @Column({ type: String, unique: true, nullable: true })
+  @Expose({ groups: ['me', 'admin'] })
+  email: string | null;
 
-  @Column('varchar', {
-    name: 'email',
-    nullable: false,
-    unique: true,
-    length: 50,
+  @Column({ nullable: true })
+  @Exclude({ toPlainOnly: true })
+  password: string;
+
+  @Exclude({ toPlainOnly: true })
+  public previousPassword: string;
+
+  @AfterLoad()
+  public loadPreviousPassword(): void {
+    this.previousPassword = this.password;
+  }
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async setPassword() {
+    if (this.previousPassword !== this.password && this.password) {
+      const salt = await bcrypt.genSalt();
+      this.password = await bcrypt.hash(this.password, salt);
+    }
+  }
+
+  @Column({ default: AuthProvidersEnum.email })
+  @Expose({ groups: ['me', 'admin'] })
+  provider: string;
+
+  @Index()
+  @Column({ type: String, nullable: true })
+  @Expose({ groups: ['me', 'admin'] })
+  socialId: string | null;
+
+  @Index()
+  @Column({ type: String, nullable: true })
+  firstName: string | null;
+
+  @Index()
+  @Column({ type: String, nullable: true })
+  lastName: string | null;
+
+  @ManyToOne(() => FileEntity, {
+    eager: true,
   })
-  email!: string;
+  photo?: FileEntity | null;
 
-  @Column('varchar', {
-    name: 'name',
-    nullable: false,
-    length: 20,
+  @ManyToOne(() => Role, {
+    eager: true,
   })
-  name!: string;
+  role?: Role | null;
 
-  @Column({
-    type: 'date',
-    name: 'birth_date',
-    nullable: true,
+  @ManyToOne(() => Status, {
+    eager: true,
   })
-  birthDate!: Date;
+  status?: Status;
 
-  @Column({
-    type: 'boolean',
-    name: 'gender',
-    nullable: true,
-  })
-  gender!: boolean;
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
 }
