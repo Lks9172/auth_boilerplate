a0e99c2bdf3c95c2cf6cccc2085d44bdf9536c3e
Lks9172 <ekdan9172@naver.com>
2024-03-08 10:16:45 +0900

    feat: AuthRegisterLoginDto정의
    - register메서드에 사용할 AuthRegisterLoginDto정의
    - email필드값에 대해 이미 이미 존재하 user email인지 검증
    - register메서드에 적용

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,13 @@
 import {
+  Post,
+  HttpCode,
+  Body,
   Controller,
+  HttpStatus,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthRegisterLoginDto } from './dto/auth-register-login.dto';
 
 @ApiTags('Auth')
 @Controller({
@@ -12,4 +17,9 @@ import { ApiTags } from '@nestjs/swagger';
 export class AuthController {
   constructor(private readonly service: AuthService) {}
 
+  @Post('email/register')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async register(@Body() createUserDto: AuthRegisterLoginDto): Promise<void> {
+    return undefined;
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,15 +6,19 @@ import { JwtModule } from '@nestjs/jwt';
 import { IsExist } from '../utils/validators/is-exists.validator';
 import { IsNotExist } from '../utils/validators/is-not-exists.validator';
 import { UserModule } from '../user/user.module';
+
 @Module({
   imports: [
     UserModule,
     PassportModule,
     JwtModule.register({}),
   ],
+  controllers: [AuthController],
   providers: [
     IsExist,
     IsNotExist,
+    AuthService,
   ],
+  exports: [AuthService],
 })
 export class AuthModule {}
