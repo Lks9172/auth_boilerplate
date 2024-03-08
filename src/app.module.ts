a0f881452551f5bcbc6e9ccac8f7fe72b7951bb6
Lks9172 <ekdan9172@naver.com>
2024-03-08 09:52:15 +0900

    feat: auth.module 생성
    - auth.module정의
    - app.module에 auth.module등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,6 +29,7 @@ import { AuthModule } from './auth/auth.module';
       },
     }),
     UserModule,
+    AuthModule
   ],
 })
 export class AppModule {}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { IsExist } from '../utils/validators/is-exists.validator';
+import { IsNotExist } from '../utils/validators/is-not-exists.validator';
+import { UserModule } from '../user/user.module';
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({}),
+  ],
+  providers: [
+    IsExist,
+    IsNotExist,
+  ],
+})
+export class AuthModule {}
