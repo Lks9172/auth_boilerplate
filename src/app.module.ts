d35f47cf702635a90dc72bdd393992e5903eb8f5
Lks9172 <ekdan9172@naver.com>
2024-03-11 02:04:16 +0900

    feat: app.module에 MailModule, MailerModule등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,14 @@ import { TypeOrmConfigService } from './database/typeorm-config.service';
 import { UserModule } from './user/user.module';
 import databaseConfig from './database/config/database.config';
 import authConfig from './auth/config/auth.config';
+import mailConfig from './mail/config/mail.config';
 import appConfig from './config/app.config';
 import fileConfig from './files/config/file.config';
 import { ConfigModule } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
   imports: [
@@ -18,6 +21,7 @@ import { AuthModule } from './auth/auth.module';
         databaseConfig,
         authConfig,
         appConfig,
+        mailConfig,
         fileConfig
       ],
       envFilePath: ['.env'],
@@ -29,7 +33,9 @@ import { AuthModule } from './auth/auth.module';
       },
     }),
     UserModule,
-    AuthModule
+    AuthModule,
+    MailModule,
+    MailerModule
   ],
 })
 export class AppModule {}
