4e142a893a2e1d6152f3da3db2c6acd106395eb5
Lks9172 <ekdan9172@naver.com>
2024-03-11 02:00:27 +0900

    feat: 회원가입 인증메일 전송을 위해 Mail, Mailer모듈, 서비스 개발
    - mail: userSignUp()메서드 작성
    - mailer: transporter멤버변수 환경변수로 초기화, send메서드에서 인증메일 템플릿 읽어 메일 전송

---
+++ src/mail/application/mail.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MailData } from '../interfaces/mail-data.interface';
+import { AllConfigType } from '../../config/config.type';
+import { MailerService } from '../../mailer/application/mailer.service';
+import path from 'path';
+
+@Injectable()
+export class MailService {
+  constructor(
+    private readonly mailerService: MailerService,
+    private readonly configService: ConfigService<AllConfigType>,
+  ) {}
+
+  async userSignUp(mailData: MailData<{ hash: string }>): Promise<void> {
+    const emailConfirmTitle = 'Confirm email';
+    const text1 = 'Hey!';
+    const text2 = 'You’re almost ready to start enjoying';
+    const text3 = 'Simply click the big green button below to verify your email address.';
+    
+
+    const url = new URL(
+      this.configService.getOrThrow('app.frontendDomain', {
+        infer: true,
+      }) + '/confirm-email',
+    );
+    url.searchParams.set('hash', mailData.data.hash);
+
+    await this.mailerService.sendMail({
+      to: mailData.to,
+      subject: emailConfirmTitle,
+      text: `${url.toString()} ${emailConfirmTitle}`,
+      templatePath: path.join(
+        this.configService.getOrThrow('app.workingDirectory', {
+          infer: true,
+        }),
+        'src',
+        'mail',
+        'mail-templates',
+        'activation.hbs',
+      ),
+      context: {
+        title: emailConfirmTitle,
+        url: url.toString(),
+        actionTitle: emailConfirmTitle,
+        app_name: this.configService.get('app.name', { infer: true }),
+        text1,
+        text2,
+        text3,
+      },
+    });
+  }
+}
