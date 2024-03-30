8301bab70302bd1f265eb423f1f958c0739a567b
Lks9172 <ekdan9172@naver.com>
2024-03-30 14:58:03 +0900

    feat: test code에 사용하는 mock user, session, socialUser객체 정의

---
+++ src/test/utils/mock-session.ts
@@ -0,0 +1,10 @@
+class MockSession {
+  id = 1;
+  user = {
+    id:1
+  };
+  createdAt = new Date();
+  updatedAt = new Date();
+}
+
+export default MockSession;
--- src/test/utils/mock-user.ts
+++ src/test/utils/mock-user.ts
@@ -1,7 +1,7 @@
 import { AuthProvidersEnum } from '../../auth/domain/auth-providers.enum';
 
 
-class MockUser {
+export class MockUser {
   id = 1;
   email = 'test@example.com';
   password = 'hashedpassword'; // 실제 테스트에서는 bcrypt로 해시된 패스워드를 사용하지 않습니다.
@@ -30,4 +30,60 @@ class MockUser {
   }
 }
 
-export default MockUser;
+export class MockSocialUser {
+  id = 1;
+  email = 'newemail@naver.com';
+  password = 'hashedpassword'; // 실제 테스트에서는 bcrypt로 해시된 패스워드를 사용하지 않습니다.
+  previousPassword = 'previoushashedpassword';
+  provider = AuthProvidersEnum.kakao;
+  socialId = '1';
+  firstName = 'John';
+  lastName = 'Doe';
+  role = {
+    id: 1,
+    name: 'User',
+  };
+  status = {
+    id: 1,
+    name: 'Active',
+  };
+  createdAt = new Date();
+  updatedAt = new Date();
+  deletedAt = null;
+
+  async setPassword() {
+    if (this.previousPassword !== this.password) {
+      // setPassword가 호출되었을 때의 간단한 행동을 구현
+      this.password = 'newhashedpassword';
+    }
+  }
+}
+
+export class MockInActiveUser {
+  id = 1;
+  email = 'test@example.com';
+  password = 'hashedpassword'; // 실제 테스트에서는 bcrypt로 해시된 패스워드를 사용하지 않습니다.
+  previousPassword = 'previoushashedpassword';
+  provider = AuthProvidersEnum.email;
+  socialId = null;
+  firstName = 'John';
+  lastName = 'Doe';
+  role = {
+    id: 1,
+    name: 'User',
+  };
+  status = {
+    id: 2,
+    name: 'inactive',
+  };
+  createdAt = new Date();
+  updatedAt = new Date();
+  deletedAt = null;
+
+  async setPassword() {
+    if (this.previousPassword !== this.password) {
+      // setPassword가 호출되었을 때의 간단한 행동을 구현
+      this.password = 'newhashedpassword';
+    }
+  }
+}
