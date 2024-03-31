39fb1236164240cb2ec007aba89df7ef4948c1fa
Lks9172 <ekdan9172@naver.com>
2024-03-31 18:54:17 +0900

    test: OAuthGoogleService.getProfileByToken메서드에 대한 테스트 코드 작성
    - OAuthKakaoService의 속성으로 getProfileByToken가 정의되어있는지 테스트 코드 작성
    - OAuthKakaoServicegetProfileByToken가 function type로 정의되어있는지 테스트 코드 작성
    - OAuthKakaoService.getProfileByToken내부에서
    의존객체의 메서드들이 호출될때 올바른 파라미터로 호출되는지 테스트 코드 작성
    - OAuthKakaoService.getProfileByToken내부에서 httpService.get메서드가 에러를 반환할때 예외처리가 잘 동작하는지 테스트 코드 작성
    - OAuthKakaoService.getProfileByToken: 정상작동할 경우 올바른 반환값을 반환하는지 테스트 코드 작성

---
+++ src/test/oauth/oauth-kakao.service.spec.ts
@@ -0,0 +1,111 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { OAuthKakaoService } from '../../oauth/application/oauth-kakao.service';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { of, throwError } from 'rxjs';
+import { AxiosResponse } from 'axios';
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+describe('OAuthKakaoService', () => {
+  let oAuthKakaoService: OAuthKakaoService;
+  let httpService: HttpService;
+  const mockData = {
+    data: {
+      id: 'some-id',
+      kakao_account: { email: 'user@example.com' },
+      properties: { nickname: 'John Doe' },
+    },
+    status: 200, // 예시로 200 OK 상태 코드 사용
+    statusText: 'OK',
+    headers: {},
+    config: {},
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        OAuthKakaoService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn().mockImplementation(() => {
+              return of(mockData as AxiosResponse);
+            }),
+          },
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key) => {
+              if (key === 'kakao.userInfoUrl') {
+                return 'https://kakao.api/user/info';
+              }
+              if (key === 'kakao.verifyTokenUrl') {
+                return 'https://kakao.api/token/verify';
+              }
+            }),
+          },
+        },
+      ],
+    }).compile();
+
+    oAuthKakaoService = module.get<OAuthKakaoService>(OAuthKakaoService);
+    httpService = module.get<HttpService>(HttpService);
+  });
+
+
+  describe('getProfileByToken', () => {  
+    const loginDto = { social: 'kakao', idToken: 'fake-id-token' };
+
+    it('should be defined', () => {
+      expect(oAuthKakaoService.getProfileByToken).toBeDefined();
+    });
+
+    it('type check', () => {
+      expect(typeof oAuthKakaoService.getProfileByToken).toBe('function');
+    });
+
+    it('check called time', async () => {
+      await oAuthKakaoService.getProfileByToken(loginDto);
+
+      expect(httpService.get).toHaveBeenCalledTimes(2);
+    });
+
+    it('check called with parameter', async () => {
+      await oAuthKakaoService.getProfileByToken(loginDto);
+
+      expect(httpService.get).toHaveBeenNthCalledWith(1, 'https://kakao.api/token/verify', expect.any(Object));
+      expect(httpService.get).toHaveBeenNthCalledWith(2, 'https://kakao.api/user/info', expect.any(Object));
+    });
+
+    it('check error: selectFile at File transfer error', async () => {
+      jest.spyOn(httpService, 'get').mockReturnValueOnce(
+        throwError(() => new Error('Token verification failed'))
+      );
+    const error422Token = new HttpException(
+        {
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          errors: {
+            user: 'wrongToken',
+          },
+        },
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+
+      const res = await oAuthKakaoService.getProfileByToken(loginDto)
+        .catch(e => e);
+      expect(res).toStrictEqual(error422Token);
+    });
+
+    it('should call verifyToken and getPayload correctly', async () => {
+      const profile = await oAuthKakaoService.getProfileByToken(loginDto);
+
+      expect(profile).toEqual({
+        id: mockData.data.id,
+        email: mockData.data.kakao_account.email,
+        firstName: '',
+        lastName: mockData.data.properties.nickname,
+      });
+    });
+  });
+});
