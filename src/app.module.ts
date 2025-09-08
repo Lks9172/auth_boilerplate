192960964b1791f97c7d2a9041d00c9a70c235cc
Lks9172 <ekdan9172@naver.com>
2025-09-08 20:41:31 +0900

    chore: 애플리케이션 설정 및 의존성 업데이트

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,10 @@ import googleConfig from './oauth/config/google/google.config';
 import kakaoConfig from './oauth/config/kakao/kakao.config';
 import naverConfig from './oauth/config/naver/naver.config';
 import { FilesModule } from './files/files.module';
+import { PermissionsModule } from './permissions/permissions.module';
+import { RolesModule } from './roles/roles.module';
+import { CommonModule } from './common/common.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
@@ -33,17 +37,28 @@ import { FilesModule } from './files/files.module';
       ],
       envFilePath: ['.env'],
     }),
+    EventEmitterModule.forRoot({
+      // 이벤트 에미터 설정
+      wildcard: false, // 와일드카드 이벤트 비활성화
+      delimiter: '.', // 이벤트 구분자
+      maxListeners: 10, // 최대 리스너 수
+      verboseMemoryLeak: false, // 메모리 누수 경고 비활성화
+      ignoreErrors: false, // 에러 무시하지 않음
+    }),
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
       dataSourceFactory: async (options: DataSourceOptions) => {
         return new DataSource(options).initialize();
       },
     }),
+    CommonModule,
     UserModule,
     FilesModule,
     AuthModule,
     MailModule,
-    MailerModule
+    MailerModule,
+    RolesModule,
+    PermissionsModule
   ],
 })
 export class AppModule {}
